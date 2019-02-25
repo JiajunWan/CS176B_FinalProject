8bc4774d199e9826c8eed156e149b736b8a09b50
jiajunwan <jiajunwan@umail.ucsb.edu>
2019-02-24 16:50:54 -0800

    Add pyYify

--- main.py
+++ main.py
@@ -23,14 +23,13 @@ def bt_address():
             urllib.request.urlopen(form.address.data)
         except urllib.error.HTTPError as e:
             error = 1
-            print('Downloading failed. Please check address', 'danger')
+            flash('Downloading failed. Please check address', 'danger')
         except urllib.error.URLError as e:
             error = 1
-            print('Downloading failed. Please check address', 'danger')
+            flash('Downloading failed. Please check address', 'danger')
         if error is not 1:
             flash('BitTorrent Download starts...', 'success')
             return redirect(url_for('home'))
-
     return render_template('bt_address.html', title='BitTorrent', form=form)
 
 
@@ -38,11 +37,22 @@ def bt_address():
 def http_address():
     form = InformationForm()
     if form.validate_on_submit():
-        if webbrowser.open(form.address.data):
-            flash('HTTP Download starts...', 'success')
-            return redirect(url_for('home'))
-        else:
+        error = 0
+        try:
+            webbrowser.open(form.address.data)
+        except webbrowser.Error as e:
+            error = 1
             flash('Downloading failed. Please check address', 'danger')
+        except webbrowser.Error as e:
+            error = 1
+            flash('Downloading failed. Please check address', 'danger')
+        if error is not 1:
+            flash('HTTP Download starts...', 'success')
+            os.system(python3 -m webbrowser )
+            return redirect(url_for('home'))
+
+
+        
     return render_template('http_address.html', title='HTTP', form=form)
 
 
